18d4479c7c27606f0b26dac68e7da9211950faad
jamesdo <jamesdo@nbp-179-45.nbp.ruw.rutgers.edu>
2013-11-04 18:52:08 -0500

    yeah br0

---
+++ search.c
@@ -0,0 +1,23 @@
+/*
+ * Brendan Parks and James Do
+ * search.c
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+// SEARCH (LOGICAL AND STATEMENT)
+
+
+
+
+
+
+
+
+
+
+// SEARCH (LOGICAL OR STATEMENT)
+
